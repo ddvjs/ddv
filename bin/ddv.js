3a003e201b83924744fcaf0f77cc9b4c8872bbf4
yuchonghua <yuchonghua@163.com>
2017-01-21 16:57:06 +0800

    JavaScript Standard Style
    标准js

--- bin/ddv.js
+++ bin/ddv.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
-'use strict';
-//标题
-process.title = 'ddvBin';
+'use strict'
+// 标题
+process.title = 'ddvBin'
 if (process.argv.indexOf('--no-run-daemon') > -1) {
-	require('../lib/daemon/index.js');
-}else{
-	require('../lib/cli/index.js');
+  require('../lib/daemon/index.js')
+} else {
+  require('../lib/cli/index.js')
 }
